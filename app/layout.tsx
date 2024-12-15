1543bb26cc4c3fe92d01145086c6e714402b8c3d
jarutosurano <jarutosurano@gmail.com>
2024-12-15 11:02:39 -0500

    Setup Auth Layouts, Social Auth Form, Next Auth, OAuth with Google and GitHub Providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import React from "react";
+import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
 
 import "./globals.css";
-import Navbar from "@/components/navigation";
+import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/toaster";
 import ThemeProvider from "@/context/Theme";
 
 const inter = localFont({
@@ -19,31 +21,35 @@ const spaceGrotesk = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "DevFlow | jarutosurano",
-  description: "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+    title: "DevFlow | jarutosurano",
+    description: "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
     icons: {
-      icon: "/images/site-logo.svg",
+        icon: "/images/site-logo.svg",
     },
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
-      <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-      >
-          <Navbar />
-          {children}
-      </ThemeProvider>
-      </body>
-    </html>
-  );
-}
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+    const session = await auth();
+
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <SessionProvider session={session}>
+            <body
+                className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+            >
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+            </ThemeProvider>
+            <Toaster />
+            </body>
+        </SessionProvider>
+        </html>
+    );
+};
+
+export default RootLayout;
